5c59c8e45a06d05774910742f151cb469c3157b3
Rui Li <rli3@caltech.edu>
2021-12-23 20:05:57 -0800

    use einsum, correct the range of summation

--- src/ci_utils.py
+++ src/ci_utils.py
@@ -25,8 +25,6 @@ def phase_factor(excitation, left_indices, right_indices):
 
 def diagonalize_ci(one_electron_integrals, two_electron_integrals, n_elecs, n_spin = 0) :
 
-    # return np.array([0,0])
-
     n_rows, n_cols = one_electron_integrals.shape
 
     n_orbs = n_rows
@@ -58,22 +56,25 @@ def diagonalize_ci(one_electron_integrals, two_electron_integrals, n_elecs, n_sp
             alpha_excitation = compare_excitation(i_alpha_combination, j_alpha_combination)
             beta_excitation = compare_excitation(i_beta_combination, j_beta_combination)
 
-            alpha_phase = phase_factor(alpha_excitation, i_alpha_combination, j_alpha_combination)
-            beta_phase = phase_factor(beta_excitation, i_beta_combination, j_beta_combination)
-
-            total_phase_factor = alpha_phase * beta_phase
-
             n_alpha_excitation = len(alpha_excitation[0])
             n_beta_excitation = len(beta_excitation[0])
 
             if n_alpha_excitation + n_beta_excitation > 2:
                 continue
 
+            alpha_phase = phase_factor(alpha_excitation, i_alpha_combination, j_alpha_combination)
+            beta_phase = phase_factor(beta_excitation, i_beta_combination, j_beta_combination)
+
+            total_phase_factor = alpha_phase * beta_phase
+
             if n_alpha_excitation + n_beta_excitation == 0:
-                hamiltonian[i, j] += sum([one_electron_integrals[p, p] for p in (list(i_alpha_combination) + list(i_beta_combination))])
-                for k in range(n_orbs):
-                    for t in range(k, n_orbs):
-                        hamiltonian[i, j] += two_electron_integrals[k, k, t, t] - two_electron_integrals[k, t, k, t]
+                concatenated = list(i_alpha_combination) + list(i_beta_combination)
+                one_electron_integral_index = np.ix_(concatenated, concatenated)
+                two_electron_integrals_index = np.ix_(concatenated, concatenated, concatenated, concatenated)
+                hamiltonian[i, j] = \
+                    np.einsum("ii->", one_electron_integrals[one_electron_integral_index]) + \
+                    0.5 * np.einsum("iijj->", two_electron_integrals[two_electron_integrals_index]) - \
+                    0.5 * np.einsum("ijji->", two_electron_integrals[two_electron_integrals_index])
 
             if n_alpha_excitation + n_beta_excitation == 1:
                 if n_alpha_excitation == 1:
@@ -84,9 +85,15 @@ def diagonalize_ci(one_electron_integrals, two_electron_integrals, n_elecs, n_sp
                     index_a = list(beta_excitation[0])[0]
                     index_b = list(beta_excitation[1])[0]
 
-                element = one_electron_integrals[index_a, index_b] + sum(
-                    [two_electron_integrals[index_a, index_b, p, p] - two_electron_integrals[index_a, p, index_b, p] for
-                     p in range(n_orbs)])
+                concatenated = \
+                    list(set(i_alpha_combination).intersection(set(j_alpha_combination))) + \
+                    list(set(i_beta_combination).intersection(set(j_beta_combination)))
+
+                coulomb_submatrix = two_electron_integrals[np.ix_([index_a], [index_b], concatenated, concatenated)]
+                exchange_submatrix = two_electron_integrals[np.ix_([index_a], concatenated, concatenated, [index_b])]
+                element = \
+                    one_electron_integrals[index_a, index_b] + np.einsum("ijkk->", coulomb_submatrix) - \
+                    np.einsum("ikkj->", exchange_submatrix)
                 hamiltonian[i, j] = total_phase_factor * element
                 hamiltonian[j, i] = total_phase_factor * element
 
@@ -101,8 +108,9 @@ def diagonalize_ci(one_electron_integrals, two_electron_integrals, n_elecs, n_sp
                 hamiltonian[i, j] = total_phase_factor * element
                 hamiltonian[j, i] = total_phase_factor * element
 
-    # print(np.sort(np.linalg.eigvals(hamiltonian)))
-    return np.sort(np.linalg.eigvals(hamiltonian))
+    # print(np.sort(np.linalg.eigvals(hamiltonian)))\
+    print(hamiltonian[:5, :5])
+    return np.sort(np.linalg.eigvals(hamiltonian)[:5])
     # return src.matrix_utils.davidson_diagonalization(hamiltonian, 2, search_dim_multiplier = 6)
 
 def addressing_array(elec_index, orb_index, n_elec, n_orb):
